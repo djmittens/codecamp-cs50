b387946826184e9f6d2a37e38fde7c2e1bbc2460
Nick Gridinskiy <nick.gridinskiy@creditkarma.com>
2022-04-21 17:45:58 -0700

    Making some progress

--- sudoku-solver/board.lua
+++ sudoku-solver/board.lua
@@ -1,5 +1,7 @@
 BLANK = -1
 
+local BIG_FONT = love.graphics.newFont(64)
+
 local _M = {
     board1 = {
         { 2, 6, BLANK, BLANK, BLANK, 3, BLANK, 1, 5 },
@@ -30,18 +32,20 @@ end
 function _M:draw()
     for i, v in pairs(self.board) do
         for k, v in pairs(v) do
-            local x = (k - 1) * 100
-            local y = (i - 1) * 100
+            local x = (k - 1) * 111 + 5
+            local y = (i - 1) * 111 + 5
 
-            love.graphics.setColor(x / 255, y / 255, 50, 1)
+            love.graphics.setColor(.75, .75, 50, 1)
             love.graphics.rectangle('fill', x , y, 100, 100)
             
             if v > 0 then
-                love.graphics.setColor(.5, .5, .5, 1)
-                love.graphics.print(v, x, y)
+                local font = BIG_FONT
+                local w = font:getWidth(v)
+                local h = font:getHeight()
+                love.graphics.setColor(.5, .0, .0, 1)
+                love.graphics.setFont(font)
+                love.graphics.print(v, x + (100 - w) / 2, y + (100 - h) / 2)
             end
-            
-            print(x, y)
         end
     end
 end
