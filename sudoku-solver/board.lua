33d7e7234385d04bfc60085e4b15978096a8306c
Dj Mittens <niksgamin@gmail.com>
2022-04-24 13:09:04 -0700

    Got some stuff  built out or something

--- sudoku-solver/board.lua
+++ sudoku-solver/board.lua
@@ -5,15 +5,15 @@ local LIL_FONT = love.graphics.newFont(24)
 local BOARD_SIZE = 9 * 9
 
 BOARD1 = {
-    2, 6, BLANK, BLANK, BLANK, 3, BLANK, 1, 5 ,
-    4, 7, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, 8 ,
+    2, 6, BLANK, BLANK, BLANK, 3, BLANK, 1, 5,
+    4, 7, BLANK, BLANK, BLANK, BLANK, BLANK, BLANK, 8,
     5, 8, 1, BLANK, BLANK, 4, 7, 6, 3,
-    BLANK, 3, BLANK, 4, 8, 9, BLANK, 7, BLANK ,
-    BLANK, BLANK, 6, BLANK, BLANK, 2, 8, 3, BLANK ,
-    BLANK, BLANK, 8, 3, 1, BLANK, BLANK, BLANK, BLANK ,
-    6, 9, BLANK, BLANK, BLANK, 8, BLANK, BLANK, 7 ,
-    6, BLANK , BLANK, BLANK, 9, BLANK, 2, BLANK, BLANK ,
-    BLANK, 1, BLANK, 5, BLANK, BLANK, BLANK, 9, 6 ,
+    BLANK, 3, BLANK, 4, 8, 9, BLANK, 7, BLANK,
+    BLANK, BLANK, 6, BLANK, BLANK, 2, 8, 3, BLANK,
+    BLANK, BLANK, 8, 3, 1, BLANK, BLANK, BLANK, BLANK,
+    6, 9, BLANK, BLANK, BLANK, 8, BLANK, BLANK, 7,
+    6, BLANK, BLANK, BLANK, 9, BLANK, 2, BLANK, BLANK,
+    BLANK, 1, BLANK, 5, BLANK, BLANK, BLANK, 9, 6,
 }
 
 local _M = {
@@ -26,20 +26,20 @@ local function unwrap(i, b)
     local offset_x = i % 9
     local offset_y = math.floor(i / 9)
 
-    for y = 0,2,1 do
-        for x = 0,2,1 do
-            table.insert(res, b[(offset_y + y) * 9 + (offset_x + x)  + 1])
+    for y = 0, 2, 1 do
+        for x = 0, 2, 1 do
+            table.insert(res, b[(offset_y + y) * 9 + (offset_x + x) + 1])
         end
     end
-    
+
     return res
 end
 
 local function update_draw(board)
     board._draw = {}
-    for k = 0,2,1 do
-        for j = 0,2,1 do
-            table.insert(board._draw, unwrap(k * 9 * 3  + (j * 3), board.board))
+    for k = 0, 2, 1 do
+        for j = 0, 2, 1 do
+            table.insert(board._draw, unwrap(k * 9 * 3 + (j * 3), board.board))
         end
     end
 end
@@ -51,10 +51,10 @@ function _M:new(board)
     }
 
     for i, v in pairs(board) do
-        if v > BLANK then 
+        if v > BLANK then
             res.board[i] = v
         else
-            res.board[i] = {1,2,3,4,5,6,7,8,9}
+            res.board[i] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
         end
     end
 
@@ -62,6 +62,11 @@ function _M:new(board)
 
     setmetatable(res, _M)
     return res
+end
+
+local function mouseon(x, y, size)
+    local m_x, m_y = love.mouse.getPosition()
+    return m_x > x and m_x < x + size and m_y > y and m_y < y + size
 end
 
 function _M:draw_squares(x, y, size, board)
@@ -80,12 +85,21 @@ function _M:draw_squares(x, y, size, board)
         local j = (i % 3) * size + x
         local k = math.floor(i / 3) * size + y
         -- draw background
-        love.graphics.setColor(j / 1000, k / 1000, .5, 1)
+        -- local scale = 1 - (size + j + k) / 2000
+        -- local scale = math.sqrt(math.pow(k, 2) + j + .5) / 2000 + .3
+        local scale
+        if type(v) == 'table' then
+            scale = .1
+        else
+            scale = .8
+        end
+
+        love.graphics.setColor(scale, scale, scale, 2)
         love.graphics.rectangle('fill', j, k, size, size)
 
-        -- if v > BLANK then 
-        if type(v) == 'table' then 
-            _M:draw_squares((j) + 5, (k) + 5, math.floor(size / 3) - 4, v)
+        -- if v > BLANK then
+        if type(v) == 'table' then
+            _M:draw_squares(j, k, math.floor(size / 3), v)
             -- print(v)
         elseif v > BLANK then
             local w = (size - font:getWidth(v)) / 2
@@ -98,17 +112,7 @@ function _M:draw_squares(x, y, size, board)
 end
 
 function _M:draw()
-    for i = 1,3*3, 1 do
-        local x = (i - 1) % 3 * 333
-        local y = math.floor((i - 1) / 3) * 333
-
-        love.graphics.setColor(x / 1000, y / 1000, .5, 1)
-        love.graphics.rectangle('fill', x, y, 333, 333)
-    end
-
-
-    _M:draw_squares(5, 5, math.floor(1000 / 3) - 4, self._draw)
-
+    _M:draw_squares(0, 0, math.floor(1000 / 3), self._draw)
 end
 
 return _M
